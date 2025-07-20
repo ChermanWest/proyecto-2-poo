2ec5d65c978836ef0e8a75e1bffd317997e9d8ee
ChermanWest <german.17.ct@gmail.com>
2025-07-20 03:12:46 -0400

    u

--- Especialidad.java
+++ Especialidad.java
@@ -1,11 +1,11 @@
-public enum TipoEspecialidad {
+public enum Especialidad {
     PEDIATRIA(25000),
     CARDIOLOGIA(35000),
     TRAUMATOLOGIA(30000);
 
     private final int precioConsulta;
 
-    TipoEspecialidad(int precioConsulta) {
+    Especialidad(int precioConsulta) {
         this.precioConsulta = precioConsulta;
     }
 
--- Medico.java
+++ Medico.java
@@ -1,20 +1,20 @@
 // Make sure Persona is imported or defined in the same package
 public class Medico extends Persona {
-    private TipoEspecialidad especialidad;
+    private Especialidad especialidad;
 
-    public Medico(String Nombre, String Rut, TipoEspecialidad especialidad) {
+    public Medico(String Nombre, String Rut, Especialidad especialidad) {
         super(Nombre, Rut);
         this.especialidad = especialidad;
     }
 
-    public TipoEspecialidad getEspecialidad() {
+    public Especialidad getEspecialidad() {
         return especialidad;
     }
 
     public int getPrecioConsulta() {
         return especialidad.getPrecioConsulta();
     }
-    public void setEspecialidad(TipoEspecialidad especialidad) {
+    public void setEspecialidad(Especialidad especialidad) {
         this.especialidad = especialidad;
     }
     public void mostrarInfo() {
